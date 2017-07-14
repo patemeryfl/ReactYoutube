1e97aaf01e310f3f5184921f05004a7637f1cd4c
Patrick Emery <Patrick@Patricks-iMac.local>
2017-07-13 21:17:26 -0400

    Initial File Upload

---
+++ src/syntax/syntaxChecker.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Patrick on 7/13/17.
+ */
+//import React from 'react';
+// import PropTypes from 'prop-types';
+
+
+/*class App extends React.Component {
+    constructor() {
+        super();
+            this.state = {
+                first: 'This is the state text'
+            }
+    }
+    updateText(e) {
+       this.setState({first: e.target.value});
+    }
+
+    render() {
+       return (
+            <div>
+                <h1>{this.state.first}</h1>
+                <Widget update={this.updateText.bind(this)}/>
+            </div>
+        )
+     }
+ }
+
+ const Widget = (props) => {
+    return <input type="text" onChange={props.update}/>;
+ };
+
+
+
+/* App.propTypes = {
+     txt: PropTypes.string,
+     cat: PropTypes.number
+ };
+
+ App.defaultProps = {
+     txt: "This is default text",
+     cat: "This is the cat box"
+ };
+
+
+export default App;
+
+
+/*const Const = () => <h1>Hello Const</h1>;*/
+/*
+ <nav className="navbar navbar-fixed-top navbar-inverse">
+ <div className="container-fluid">
+ <div className="navbar-header">
+ <a className="navbar-brand" href="">WebSiteName</a>
+ </div>
+ <ul className="nav navbar-nav">
+ <li className="active"><a href="">Home</a></li>
+ <li><a href="">Link</a></li>
+ <li><a href="">Link</a></li>
+ </ul>
+ <button className="btn btn-danger navbar-btn">Button</button>
+ </div>
+ </nav>*/
---
+++ src/views/video-detail.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Patrick on 7/13/17.
+ */
